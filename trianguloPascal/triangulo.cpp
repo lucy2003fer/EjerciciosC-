6145a8c0b285879bc26b45b07b89f57ff1814753
lucy2003fer <lucy48fernada@gmail.com>
2025-02-02 15:40:52 -0500

    esta funcion crea el triangulo de pasacal con puntos segun un numero dado

---
+++ trianguloPascal/triangulo.cpp
@@ -0,0 +1,14 @@
+#include "triangulo.hpp"
+#include <iostream>
+using namespace std;
+void triangulol(int alt){
+    for(int i = 1 ; i <= alt ; i++){
+        for(int j = 1 ; j <= alt - i ; j++ ){
+            cout << " ";
+        }
+        for(int k = 1 ; k <= (i * 2) - 1 ; k++){
+            cout << ".";
+        }
+        cout << endl;
+    }
+}
