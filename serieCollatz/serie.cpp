60c49d074821c59b6b4bde25f5c872936e1538ae
lucy2003fer <lucy48fernada@gmail.com>
2025-02-04 15:22:01 -0500

    corregí un pequeño error de codigo en el archivo serie.cpp, copilé los archivos y quedo funcionando

--- serieCollatz/serie.cpp
+++ serieCollatz/serie.cpp
@@ -1,12 +1,15 @@
 #include "serie.hpp"
 #include <iostream>
 
-using namespace std;
-int main(){
-    int x;
-    cout << "Ingrese el valor a hacer la serie: ";
-    cin >> x;
-    int calculos = serie(x);
-    cout << "El numero de calculos fue: " << calculos << endl;
-    return 0;
+int serie(int x) {
+    int count = 0;
+    while (x != 1) {
+        if (x % 2 == 0) {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        count++;
+    }
+    return count;
 }
